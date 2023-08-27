94df366a5350058f3335632cabfa2da870837793
1AT1 <antonytome69@gmail.com>
2023-08-28 00:51:12 +0100

    Init
    Initialized the fork by running the following:
    
    yarn install
    
    yarn dev

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f478a2e92301939b"; }
+/******/ 		__webpack_require__.h = function() { return "ddebd271b45f99ae"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
