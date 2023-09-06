6677186ea983640d7ef812e0f03b07a3adcf89d4
Your Name <antonytome69@gmail.com>
2023-09-06 17:48:18 +0100

    ENV file
    created a .env file as i saw that variabvle were being obtained from this file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ddebd271b45f99ae"; }
+/******/ 		__webpack_require__.h = function() { return "da230d23d4a2409b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
